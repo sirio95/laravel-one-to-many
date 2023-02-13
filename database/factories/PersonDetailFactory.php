00903cd13d36547bbc3e5704a4968590ff137df2
sirio95 <sirio.iasevoli@gmail.com>
2023-02-13 17:19:03 +0100

    impostazione faker

---
+++ database/factories/PersonDetailFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PersonDetailFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'phone_num' => fake()->phoneNumber(),
+            'addess' => fake()->streetAddress(),
+            'avatar' => 'https://picsum.photos/800/600?random=' . fake()->randomNumber(5, true),
+        ];
+    }
+}
